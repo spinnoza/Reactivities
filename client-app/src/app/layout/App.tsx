a253f2d2bc2dfdc6cf14abd8f890bfc2ebc35fe3
Leo Wang <wangjil@delawareconsulting.com>
2023-01-04 17:58:32 +0800

    完成view页面

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,39 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header } from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
 import List from 'semantic-ui-react/dist/commonjs/elements/List';
 import ListItem from 'semantic-ui-react/dist/commonjs/elements/List/ListItem';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
 
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
-    axios.get('http://localhost:5039/api/Activities').then(
+    axios.get<Activity[]>('http://localhost:5039/api/Activities').then(
       response => {
         setActivities(activities => response.data);
       }
     )
-  },[]);
-  
+  }, []);
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(selectedActivity => activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelect() {
+    setSelectedActivity(selectedActivity => undefined);
+  }
+
   return (
-    <div>
-       <Header as='h2' icon='users' content='Reactivities' />
-     
-        <List>
-          <ListItem>
-            {activities.map((activity:any) => {
-              return (
-                <li key={activity.id} >
-                  {activity.title}
-                </li>
-              );
-            })}
-          </ListItem>
-       </List>
-       
-    
-    </div>
+    <>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelect}
+        />
+      </Container>
+    </>
   );
 }
 
