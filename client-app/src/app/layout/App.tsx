4c0508becfb40e1dfcab54aeca9928d9bd4e2541
Leo Wang <wangjil@delawareconsulting.com>
2023-01-20 22:36:14 +0800

    用户登录

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,10 +5,24 @@ import HomePage from '../../features/home/homePage';
 import { useLocation } from 'react-router';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { useStore } from '../stores/store';
+import { useEffect } from 'react';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const location = useLocation();
 
+  const { commonStore, userStore } = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded())
+    } else {
+      commonStore.setAppLoaded()
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
   return (
     <>
       <ToastContainer position='bottom-right' hideProgressBar />
